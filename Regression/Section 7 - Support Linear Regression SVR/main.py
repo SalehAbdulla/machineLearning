e08250456dd1e0f902636a8ed0bb10cfaa2dfd75
java <salehabdullaict@gmail.com>
2025-01-29 14:58:32 +0300

    Support Vector regression

--- Regression/Section 7 - Support Linear Regression SVR/main.py
+++ Regression/Section 7 - Support Linear Regression SVR/main.py
@@ -1,17 +1,17 @@
-# Support Linear Regression
-# import libraries
-
+# Support preprocessing vector
+# Import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 # import dataset
-dataset = pd.read_csv("Position_Salaries.csv")
+dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values  # Features
-y = dataset.iloc[:, 2:].values  # Dependent Vector
+y = dataset.iloc[:, -1].values  # Dependent Vector
+y = y.reshape(len(y), 1)  # or [:, 1:2]
 
 
-# Feature Scaling - Reshaping the data to be at the same range
+# Feature Scaling reshaping data range to -3 to 3
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
@@ -21,10 +21,14 @@ y = sc_y.fit_transform(y)
 
 # Training the SVR model on the whole dataset
 from sklearn.svm import SVR
-regressor = SVR(kernel='rbf')  # Radial Basis function
+regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
 
+# Predicting a new result
+predict = sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1))
 
-# Predict a new result
-print(sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1)))
+# Visualisation SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)), color='blue')
+plt.show()
 
